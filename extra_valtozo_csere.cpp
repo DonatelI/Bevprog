69bd715bec702b142c556ffee9de329e807ba7f7
Király Ákos <kiralyakos99@gmail.com>
2018-09-21 14:36:30 +0300

    Add files via upload

---
+++ extra_valtozo_csere.cpp
@@ -0,0 +1,17 @@
+
+int main(){
+
+    int a=5, b=2;
+
+    //valtozok csereje külonbseggel:
+    a=a+b;
+    b=a-b;
+    a=a-b;
+
+    //szorzattal:
+    a=a*b;
+    b=a/b;
+    a=a/b;
+
+    return 0;
+}
